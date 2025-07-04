c0188c60ffff233eb36208f34ddbc4bf78adcd02
mspvll <masha.pav.nov1@gmail.com>
2025-07-04 10:57:52 +0300

    states_output

--- states.py
+++ states.py
@@ -27,8 +27,9 @@ async def procces_budget(message: Message,state:FSMContext):
     list1 = []
     for city,cost in budget_list:
         list1.append(city)
+    output = str('\n'.join(str(x) for x in list1))
     if len(list1) == 0:
         await message.answer(text='К сожалению, у меня нет предложений за такую сумму 😥')
     else:
-        await message.answer(text=f'Вам подойдут туры в такие города, как:\n<b>{str('\n'.join(str(x) for x in list1))}</b> \nПодробнее о городах можешь узнать, нажав на кнопку <b>"start"</b>', parse_mode='HTML')
+        await message.answer(text=f'Вам подойдут туры в такие города, как:\n<b>{output}</b> \nПодробнее о городах можешь узнать, нажав на кнопку <b>"start"</b>', parse_mode='HTML')
     await state.clear()
